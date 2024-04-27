104656edf16452dd466147f6bd20734b749c8f94
Github <1300327964@qq.com>
2024-04-27 20:45:23 +1000

    data load

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import re
 import pandas as pd
 from io import StringIO
 
-
-
+def load_data(path):
+    with open(path) as file:
+        lines = [re.sub(r'([^,])"(\s*[^\n])', r'\1/"\2', line) for line in file]
+    df = pd.read_csv(StringIO(''.join(lines)), escapechar="/")
+    # print(df)
+    return df
 
 if __name__ == "__main__":
     FILENAME = 'train.csv'
-    with open(FILENAME) as file:
-        lines = [re.sub(r'([^,])"(\s*[^\n])', r'\1/"\2', line) for line in file]
-    df = pd.read_csv(StringIO(''.join(lines)), escapechar="/")
-    print(df)
+    df = load_data(FILENAME)
